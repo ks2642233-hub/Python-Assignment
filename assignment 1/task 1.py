797db7665167f1891620853a981de0338a58c37e
Krishn SONI <ks2642233@gmail.com>
2025-09-06 16:07:14 +0530

    Add files via upload

---
+++ assignment 1/task 1.py
@@ -0,0 +1,11 @@
+#mathematical oprestion
+num1 = int(input("Enter first  number: "))
+num2 = int(input("Enter second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("Addition: ", addition,)
+print("subtraction: ", subtraction,)
+print("multiplication: ", multiplication)
+print("division: ", division)
---
+++ assignment 1/task 2.py
@@ -0,0 +1,4 @@
+first_name  = input("Enter first name: ")
+last_name = input("   Enter second name: ")
+full_name = first_name + " " + last_name
+print("hello", full_name," !welcome to python program")
